25e391e0ebc6773d5685bcdb6017401840d2d181
jiaxcuit <2454550030@qq.com>
2022-07-06 16:53:53 +0100

    started new flutter course; trying to calculate the total score now

---
+++ flutter_course_new/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_course_new/quiz.dart';
+
+import 'quiz.dart';
+import 'results.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 4},
+        {'text': 'White', 'score': 3}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 4},
+        {'text': 'Snake', 'score': 8},
+        {'text': 'Elephant', 'score': 6},
+        {'text': 'Lion', 'score': 10}
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 10},
+        {'text': 'Max', 'score': 10},
+        {'text': 'Max', 'score': 10},
+        {'text': 'Max', 'score': 10}
+      ]
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    setState(() {
+      _questionIndex++;
+      _totalScore += score;
+      print(_questionIndex);
+    });
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+      ;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+              )
+            : Result(),
+      ),
+    );
+  }
+}
+
+// variables inside of class - properties
+// functions inside of class - methods
+// e.g build is a method of StatelessWidget
